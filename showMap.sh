f990b89e3e6ebaa437e97b338e66be91b53d5dcf
dzon2000 <dzon2000@protonmail.com>
2020-09-26 10:59:24 +0200

    Adding stats part 1

--- showMap.sh
+++ showMap.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-
+#awk '/time/, /time/ {print substr($1, 7, 19)}' tracks/29-Aug-2020-1742.gpx
 DATA=`awk 'match($0, /lat=["0-9\.]+ lon=["0-9\.]+/) {split(substr($0, RSTART, RLENGTH),a,"\""); print "[",a[2],",",a[4],"],"}' $1`
 
 echo "var latlngs=[${DATA%?}];" > data.js
