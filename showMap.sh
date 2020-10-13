5591d9215d3e6d1ba71bfc16b7b323a0b8ada6c9
dzon2000 <dzon2000@protonmail.com>
2020-10-13 17:05:05 +0200

    Adding new tracks, speed info, and speed graph

--- showMap.sh
+++ showMap.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
-#awk '/time/, /time/ {print substr($1, 7, 19)}' tracks/29-Aug-2020-1742.gpx
 
-TIME=`awk '/time/, /time/ {print substr($1, 7, 19)}' $1 | \
-	((date --date $(head -n1) +%s) ; \
+TIME=`awk '/time/, /time/ {print substr($1, 7, 19)}' $1`
+echo "var time=[`echo $TIME | awk 'NR > 1 {printf ("%s",",")} {printf ("\\"%s\\"", $0)}'`]" > data.js
+DURATION=`stdbuf -oL echo $TIME | \
+	((date --date $(head -n1) +%s) && \
 	(date --date $(tail -n1) +%s)) | \
 	awk 'NR > 1 { print $0 - prev } { prev = $0 }'`
 
-echo "var dur=$TIME;" > data.js 
+echo "var dur=$DURATION;" >> data.js 
 
 DATA=`awk 'match($0, /lat=["0-9\.]+ lon=["0-9\.]+/) {split(substr($0, RSTART, RLENGTH),a,"\""); print "[",a[2],",",a[4],"],"}' $1`
 
